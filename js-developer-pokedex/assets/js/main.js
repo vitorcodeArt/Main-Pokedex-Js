20e6b3034e26795940434ef837bda29603bda746
Vitor de Menezes <v.m.moraes1997@gmail.com>
2022-11-18 20:49:37 -0300

    Finalizando o projeto da pokedex

--- js-developer-pokedex/assets/js/main.js
+++ js-developer-pokedex/assets/js/main.js
@@ -3,20 +3,20 @@ const loadMoreButton = document.getElementById('loadMoreButton')
 const background = ".background"
 
 
-const maxRecords = 386
+const maxRecords = 493
 const limit = 50
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li id="${pokemon.name}" class="pokemon ${pokemon.name} ${pokemon.type}" onclick="getPokemon(${pokemon.name})">
+        <li id="${pokemon.id}" class="pokemon ${pokemon.type}" onclick="getPokemon(${pokemon.id})">
             <span class="number">${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type.name}">${type}</li>`).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
-                <img src="${pokemon.photo}"
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemon.id}.gif"
                      alt="${pokemon.name}">
             </div>
         </li>
@@ -27,7 +27,6 @@ function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
-        console.log(pokemons);
     })
 }
 
@@ -51,12 +50,6 @@ loadMoreButton.addEventListener('click', () => {
 
         /*------- Manipulação Html na section de caracteristicas -------*/
 
-
-
-function convertTypes(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
 function convertNum(num, pokeId) {
     if (num.length < 2) {
         num = `#00${pokeId}`
@@ -68,72 +61,116 @@ function convertNum(num, pokeId) {
     return num
 }
 
-function convertAbility(pokemonAbilities) {
-    return pokemonAbilities.map((abilitySlot) => `<li class="ability">${abilitySlot.ability.name}</li>`)
-}
-
-
-function convertDetail(pokemon) {
+function convertDetail(pokemon, specie) {
     return `
-    <div class="background ${pokemon.types[0].type.name}">
+    <div class="background ${pokemon.type}">
         <span class="return" onclick="voltar()">⟵</span>
         <div id="all-details" class="details-basic">
              <div class="division">
                  <div class="name-type">
                      <span class="name">${pokemon.name}</span>
                      <ol class="types">
-                     ${convertTypes(pokemon.types).join('')}
+                     ${pokemon.types.map((type) => `<li class="type ${type.name}">${type}</li>`).join('')}
                      </ol>
                  </div>
                  <div class="number-pokemon">
-                     <span class="number">${convertNum(pokemon.id.toString().split(''), pokemon.id)}</span>
+                     <span class="number">${pokemon.number}</span>
                  </div>
              </div>
-         </div>
-             <span class="pokemon-img">
-                 <img id="pokemon-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg" alt="bulbasaur">
-             </span>
+             </div>
              <div class="details-avanced">
+             <span class="pokemon-img">
+                 <img id="pokemon-img" class="poke-img" src="${pokemon.photo}" alt="${pokemon.name}">
+             </span>
                  <ul class="informations">
-                     <li class="information select">About</li>
-                     <li class="information">Base Stats</li>
-                     <li class="information">Evolution</li>
-                     <li class="information">Moves</li>
+                     <li id="click-about" class="information select" onclick="about()">About</li>
+                     <li id="click-stats" class="information" onclick="stats(${pokemon.id})">Base Stats</li>
                  </ul>
                  <div class="info-about">
                      <ul class="info-pokemon-left">
-                         <li>Species</li>
+                         <li>Base exp.</li>
                          <li>Height</li>
                          <li>Weight</li>
                          <li>Abilities</li>
                          <li><h4>Breeding</h4></li>
                          <li>Gender</li>
                          <li>Egg groups</li>
-                         <li>Egg cycle</li>
+                         <li>Capture rate</li>
                      </ul>
                      <ul class="info-pokemon-right">
-                         <li id="species" class="info-right">${pokemon}</li>
+                         <li id="base_experience" class="info-right">${pokemon.base_experience}</li>
                          <li id="height" class="info-right">(${(pokemon.height) * (10)
                         })cm</li>
                          <li id="weight" class="info-right">${((pokemon.weight / 10) * 2.205).toFixed(1)} lbs (${(pokemon.weight) / (10)})kg</li>
-                         <li id="species" class="info-right">
-                            <ul class="abilities">
-                            ${convertAbility(pokemon.abilities).join('')}
+                         <li id="abilities" class="info-right">
+                            <ul class="ulSloties">
+                                ${pokemon.abilities.map((ability) => `<li class="liSlot ${ability.name}">${ability}</li>`).join('')}
                             </ul>
                          </li>
                          <li class="info-right"><h4>.</h4></li>
                          <li class="info-right">
                              <ul class="gender">
-                                 <li>87.5%</li>
-                                 <li>12.5%</li>
+                                 <li><img class="img-gender" src="imgs/sexo-masculino.png" alt="simbolo do gênero masculino">  ${specie.male}</li>
+                                 <li><img class="img-gender" src="imgs/sexo-feminino.png" alt="simbolo do gênero feminino"> ${specie.female}</li>
                              </ul>
                              </li class="info-right">
-                             <li  id="egg-groups" class="info-right">${pokemon}</li>
-                         <li  id="egg-cycle" class="info-right">Grass</li>
+                             <li  id="egg-groups" class="info-right">
+                                <ul class="ulSloties">
+                                ${specie.egg_groups.map((group) => `<li id="egg_group" class="liSlot">${group}</li>`).join('')}
+                                </ul>
+                             </li>
+                         <li  id="capture_rate" class="info-right">${specie.capture_rate}</li>
                      </ul>
                  </div>
-             </div>
-    </div>
+                 <div class="info-base-stats">
+                     <div class="grid-conteiner">
+                         <ul class="info-pokemon-left">
+                             <li>HP</li>
+                             <li>Attack</li>
+                             <li>Defense</li>
+                             <li>Sp. Atk</li>
+                             <li>Sp. Def</li>
+                             <li>Speed</li>
+                             <li>Total</li>
+                         </ul>
+                         <ul id="info-stats" class="info-pokemon-right">
+                             <ul class="stats-progess">
+                                 <li class="info-right">${pokemon.hp}</li>
+                                 <li class="progess-bar"><i class="${pokemon.type}" id="hp"></i></li>
+                             </ul>
+                             <ul class="stats-progess">
+                                 <li class="info-right">${pokemon.atk}</li>
+                                 <li class="progess-bar"><i class="${pokemon.type}" id="atk"></i></li>
+                             </ul>
+                             <ul class="stats-progess">
+                                 <li class="info-right">${pokemon.def}</li>
+                                 <li class="progess-bar"><i class="${pokemon.type}" id="def"></i></li>
+                             </ul>
+                             <ul class="stats-progess">
+                                 <li class="info-right">${pokemon.sp_atk}</li>
+                                 <li class="progess-bar"><i class="${pokemon.type}" id="sp-atk"></i></li>
+                             </ul>
+                             <ul class="stats-progess">
+                                 <li class="info-right">${pokemon.sp_def}</li>
+                                 <li class="progess-bar"><i class="${pokemon.type}" id="sp-def"></i></li>
+                             </ul>
+                             <ul class="stats-progess">
+                                 <li class="info-right">${pokemon.speed}</li>
+                                 <li class="progess-bar"><i class="${pokemon.type}" id="speed"></i></li>
+                             </ul>
+                            <ul class="stats-progess">
+                                <li class="info-right">${pokemon.total}</li>
+                                <li class="progess-bar"><i class="${pokemon.type}" id="total"></i></li>
+                            </ul>
+                                             </ul>
+                     </div>
+                    <div class="div-curiosity">
+                        <h3 class="curiosity">Curiosity</h3>
+                        <p class="curiosity text">${specie.flavor_text_entries}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     `
 }
--- js-developer-pokedex/assets/js/poke-api.js
+++ js-developer-pokedex/assets/js/poke-api.js
@@ -5,6 +5,8 @@ const pokeApi = {}
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = convertNum(pokeDetail.id.toString().split(''), pokeDetail.id)
+    pokemon.id = pokeDetail.id
+
     pokemon.name = pokeDetail.name
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
@@ -24,8 +26,9 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
     }
 
-pokeApi.getPokemons = (offset = 0, limit = 20) => {
+pokeApi.getPokemons = (offset = 0, limit = 50) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    
 
     return fetch(url)
         .then((response) => response.json())
@@ -33,4 +36,5 @@ pokeApi.getPokemons = (offset = 0, limit = 20) => {
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
-}
+    }
+
